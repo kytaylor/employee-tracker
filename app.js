fac3590cad6719a3c147ef41b421a7baa820bf3d
kytaylor <ky.taylor5468@gmail.com>
2021-02-11 15:24:11 -0800

    adds seeds.sql fil, adds initial connection code in app.js

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 // MAKE SURE NOT TO COMMIT WITH PASSWORD ENTERED
+
+// Dependencies
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const consoleTable = require("console.table");
+
+// Connection Properties
+const connection = {
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "employees_DB"
+}
+
+// Database Connection
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    initMenu();
+});
+
+function initMenu() {
+    
+}
