33ce084ef0f2fd988d765b628f4cc4e2115074ac
kytaylor <ky.taylor5468@gmail.com>
2021-02-11 16:06:51 -0800

    adds inquirer questions and switch statement for answers

--- app.js
+++ app.js
@@ -22,5 +22,54 @@ connection.connect(function(err) {
 });
 
 function initMenu() {
-    
+    inquirer.prompt([{
+        name: "menu",
+        type: "list",
+        message: "What would you like to do?",
+        choices :[
+            "View all employees",
+            "View employees by role",
+            "View employees by department",
+            "Add new employee",
+            "Add new role",
+            "Add new department",
+            "Edit employee",
+            "Edit role",
+            "Edit department"
+        ]
+    }]).then(function(res) {
+        switch (res.menu) {
+            case "View all employees":
+                employeeSummary();
+                break;
+            
+            case "View employees by role":
+                roleSummary();
+                break;
+                
+            case "View employees by department":
+                departmentSummary();
+                break;
+            
+            case "Add new employee":
+                newEmployee();
+                break;
+
+            case "Add new role":
+                newRole()
+                break;
+                
+            case "Edit employee":
+                editEmployee();
+                break;
+                    
+            case "Edit role":
+                editRole();
+                break;
+                
+            case "Edit department":
+                editDepartment();
+                break;
+        }
+    })
 }
