73c9474f2d51c172cfc6432666849775842ed134
kytaylor <ky.taylor5468@gmail.com>
2021-04-02 18:00:53 -0700

    adds gif of app use

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
+  password: "Serperi0r!5468",
   database: "employees_DB",
 });
 
